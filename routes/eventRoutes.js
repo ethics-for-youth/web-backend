6eb6eef171245d1158c302b54710bb634117059c
Mohdhussain28 <md28md.hussain@gmail.com>
2025-07-12 13:11:46 +0530

    remove old code

--- routes/eventRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addEvent, fetchEvents } = require('../controllers/eventController');
-const router = express.Router();
-
-router.post('/event', addEvent);
-router.get('/events', fetchEvents);
-
-module.exports = router;
--- routes/registrationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { handleRegistration } = require('../controllers/registrationController.js');
-
-const router = express.Router();
-
-router.post('/register', handleRegistration);
-
-module.exports = router;
