724cd020ab949a8870746aef87f0943a46a28460
mohammadn0man <mohammad.noman@live.in>
2025-07-29 17:38:44 +0530

    fix: select workspace grep issue

--- terraform/backend-setup/main.tf
+++ terraform/backend-setup/main.tf
@@ -79,9 +79,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state_pab" {
 
 # DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = local.env_config.dynamodb_table_name
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = local.env_config.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
