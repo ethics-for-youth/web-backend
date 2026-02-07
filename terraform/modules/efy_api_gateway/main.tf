ae7162ded13d256912e86718f6d82857b59d8dd9
Mohdhussain28 <md28md.hussain@gmail.com>
2026-02-08 00:58:15 +0530

    Update volunteers resource path from "apply" to "join"

--- terraform/modules/efy_api_gateway/main.tf
+++ terraform/modules/efy_api_gateway/main.tf
@@ -230,7 +230,7 @@ resource "aws_api_gateway_resource" "volunteers" {
 resource "aws_api_gateway_resource" "volunteers_apply" {
   rest_api_id = aws_api_gateway_rest_api.efy_api.id
   parent_id   = aws_api_gateway_resource.volunteers.id
-  path_part   = "apply"
+  path_part   = "join"
 }
 
 resource "aws_api_gateway_resource" "volunteers_id" {
